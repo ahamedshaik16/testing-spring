5baf2ff359510a6940c83a4583cb6a4d4cf5629e
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-20 08:47:16 +0530

    Using Component Scan in JUnit5

--- src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProducer.java
+++ src/main/java/org/springframework/samples/petclinic/sfg/LaurelWordProducer.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class LaurelWordProducer implements WordProducer{
     @Override
     public String getWord() {
