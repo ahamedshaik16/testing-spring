ff91f7dc857ae8d77c30f547f67b051069abe451
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-20 08:39:10 +0530

    Initial SetUp

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterLaurelTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.sfg.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.BaseConfig;
+import org.springframework.samples.petclinic.LaurelConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
+class HearingInterpreterLaurelTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIheard() {
+        String word = hearingInterpreter.whatIheard();
+        assertEquals("Laurel", word);
+    }
+}
